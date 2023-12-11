e781f62781c87dd760ecacc519fe1e0aa7ffe7a2
SPG2001 <spgabel2@mail.fhsu.edu>
2023-12-11 15:53:27 -0600

    Added idea file to gitignore.
    Created the general bot framework.

---
+++ botmain.py
@@ -0,0 +1,56 @@
+import pyautogui as pag
+import time
+
+def combatAI():
+    #look at the screen and find out how many pips the wizard and enemies have
+
+    #Look at the screen to see how much health everybody has
+
+    #Look at the screen and look at the spells you have
+
+    #Calculate which spell to cast and on who
+
+    #Select target and cast
+
+    #loop from here
+
+
+def cardReader():
+
+    #Attach card images in hand to values for combatAI
+
+    #Keep track of what cards remain in deck
+
+
+def pathFinder():
+
+    #Keep track of quest arrow direction and move based upon it.
+
+    #Keep track of whether player is in battle or not.
+
+    #Keep track of what the quest wants
+
+    #If an interaction occurs, activate the interactionAutomator.
+
+
+def interactionAutomator():
+
+    #Navigate interactions based upon what they are.
+
+
+def inventoryTracker():
+
+    #Keep track of player inventory and be able to make comparisons of equipment.
+
+    #Used for both equipment and drop rate data
+
+
+def dropTracker():
+
+    #Use the inventoryTracker to make comparisons from the inventory.
+
+    #Store the data inside a pandas dataframe, which will be passed onto the API.
+
+def botWrapper():
+
+    #bundles the bot functions into one process.
