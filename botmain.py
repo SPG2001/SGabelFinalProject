96f7ab1d68a71fd25d9f5f0d2e4b3a1a031be374
SPG2001 <spgabel2@mail.fhsu.edu>
2023-12-12 13:09:21 -0600

    In progress for OOP. Commit to ensure if something goes wrong, it won't be hard to return to an older version.

--- botmain.py
+++ botmain.py
@@ -23,6 +23,26 @@ import os
 
 def cardReader():
     #Look at what cards are in hand and make a list
+
+    #Create card class. This will allow the stats and schools of each card to follow this formula
+    class attackcard():
+        def __init__(self, school, mindamage, maxdamage, castchance):
+            self.school = school
+            self.mindamage = mindamage
+            self.maxdamage = maxdamage
+            self.castchance = castchance
+
+    bloodbat = attackcard('myth',70, 110,0.8)
+    itembloodbat = attackcard('myth',60,100,0.8)
+    itemdarksprite = attackcard('death',55,95,0.85)
+    itemfirecat = attackcard('fire',70,110,0.75)
+    itemfrostbeetle = attackcard('ice',55,95,0.8)
+    itemimp = attackcard('life', 55,95,0.9)
+    itemscarab = attackcard('balance',55,95,0.85)
+    itemthundersnake = attackcard('storm',95,135,0.7)
+
+
+
     #Create an empty list
     cardlist = []
 
@@ -32,44 +52,79 @@ def cardReader():
     #Look at the hand and see what makes a match. If a match happens, the coordinates are put into a list.
     #If not on screen, the value is zero.
     try:
-        bloodbat = pag.locateOnScreen('bloodbat.jpg', confidence=0.8)
-        cardlist.append(bloodbat)
-    except: bloodbat = 0
+        class bloodbat():
+            bloodbatlocation = pag.locateOnScreen('bloodbat.jpg', confidence=0.8)
+            mindamage = 70
+            maxdamage = 110
+            castchance = 0.8
+        cardlist.append(bloodbat.bloodbatlocation)
+    except: print("bloodbat not found")
     try:
-        itembloodbat = pag.locateOnScreen('itembloodbat.jpg', confidence=0.8)
-        cardlist.append(itembloodbat)
-    except: itembloodbat = 0
+        class itembloodbat():
+            itembloodbatlocation = pag.locateOnScreen('itembloodbat.jpg', confidence=0.8)
+            mindamage = 60
+            maxdamage = 100
+            castchance = 0.8
+        cardlist.append(itembloodbat.itembloodbatlocation)
+    except: print("item blood bat not found")
     try:
-        itemfirecat = pag.locateOnScreen('itemfirecat.jpg', confidence=0.8)
-        cardlist.append(itemfirecat)
-    except: itemfirecat = 0
+        class itemdarksprite():
+            itemdarkspritelocation = pag.locateOnScreen('itemdarksprite.jpg', confidence=0.8)
+            mindamage = 55
+            maxdamage = 95
+            castchance = 0.85
+        cardlist.append(itemdarksprite.itemdarkspritelocation)
+    except: print("item dark sprite not found")
     try:
-        itemdarksprite = pag.locateOnScreen('itemdarksprite.jpg', confidence=0.8)
-        cardlist.append(itemdarksprite)
-    except: itemdarksprite = 0
+        class itemfirecat():
+            itemfirecatlocaton = pag.locateOnScreen('itemfirecat.jpg', confidence=0.8)
+            mindamage = 70
+            maxdamage = 110
+            castchance = 0.75
+        cardlist.append(itemfirecat.itemfirecatlocation)
+    except: print("item firecat not found")
     try:
-        itemimp = pag.locateOnScreen('itemimp.jpg', confidence=0.8)
-        cardlist.append(itemimp)
-    except: itemimp = 0
+        class itemfrostbeetle():
+            itemfrostbeetlelocation = pag.locateOnScreen('itemfrostbeetle.jpg', confidence=0.8)
+            mindamage = 55
+            maxdamage = 95
+            castchance = 0.8
+        cardlist.append(itemfrostbeetle.itemfrostbeetlelocation)
+    except: print("item frost beetle found")
     try:
-        itemstormsnake = pag.locateOnScreen('itemthundersnake.jpg', confidence=0.8)
-        cardlist.append(itemstormsnake)
-    except: itemstormsnake = 0
+        class itemimp():
+            itemimplocation = pag.locateOnScreen('itemimp.jpg', confidence=0.8)
+            mindamage = 55
+            maxdamage = 95
+            castchance = 0.9
+        cardlist.append(itemimp.itemimplocation)
+    except: print("item imp not found")
     try:
-        itemfrostbeetle = pag.locateOnScreen('itemfrostbeetle.jpg', confidence=0.8)
-        cardlist.append(itemfrostbeetle)
-    except: itemfrostbeetle = 0
+        class itemscarab():
+            def __init__(self, location, mindamage, maxdamage, castchance):
+                self.location = pag.locateOnScreen('itemscarab.jpg', confidence=0.8)
+                self.mindamage = 55
+                self.maxdamage = 95
+                self.castchance = 0.85
+                cardlist.append(itemscarab.__init__(location))
+    except: print("item scarab not found")
 
     try:
-        itemscarab = pag.locateOnScreen('itemscarab.jpg', confidence=0.8)
-        cardlist.append(itemscarab)
-
-    except: itemscarab = 0
+        class card():
+            def __init__(self, school, mindamage, maxdamage, castchance):
+                self.school = school
+                self.mindamage = mindamage
+                self.maxdamage = maxdamage
+                self.castchance = castchance
+    except: print("item thunder snake not found")
 
     try:
-        itemheartbeat = pag.locateOnScreen('itemheartbeat.jpg', confidence=0.8)
-        cardlist.append(itemheartbeat)
-    except: itemheartbeat = 0
+        class itemheartbeat():
+            itemheartbeatlocation = pag.locateOnScreen('itemheartbeat.jpg', confidence=0.8)
+            heal = 245
+            castchance = 0.9
+        cardlist.append(itemheartbeat.itemheartbeatlocation)
+    except: print("item heartbeat not found")
 
     print(cardlist)
 
