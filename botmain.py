01e1d68a2cc0e0f843e3b3d34926e628684b8129
SPG2001 <spgabel2@mail.fhsu.edu>
2023-12-12 12:00:27 -0600

    Added card images. More will come.
    botmain.py can now see cards.

--- botmain.py
+++ botmain.py
@@ -1,7 +1,13 @@
+import pyautogui
 import pyautogui as pag
 import time
+import os
 
-def combatAI():
+#def combatReader():
+    #Read the enemy health cards and communicate with the SQLite Database for the stats.
+
+
+#def combatAI():
     #look at the screen and find out how many pips the wizard and enemies have
 
     #Look at the screen to see how much health everybody has
@@ -16,13 +22,59 @@ def combatAI():
 
 
 def cardReader():
+    #Look at what cards are in hand and make a list
+    #Create an empty list
+    cardlist = []
 
-    #Attach card images in hand to values for combatAI
+    #buffer to allow swap to game
+    time.sleep(5)
 
-    #Keep track of what cards remain in deck
+    #Look at the hand and see what makes a match. If a match happens, the coordinates are put into a list.
+    #If not on screen, the value is zero.
+    try:
+        bloodbat = pag.locateOnScreen('bloodbat.jpg', confidence=0.8)
+        cardlist.append(bloodbat)
+    except: bloodbat = 0
+    try:
+        itembloodbat = pag.locateOnScreen('itembloodbat.jpg', confidence=0.8)
+        cardlist.append(itembloodbat)
+    except: itembloodbat = 0
+    try:
+        itemfirecat = pag.locateOnScreen('itemfirecat.jpg', confidence=0.8)
+        cardlist.append(itemfirecat)
+    except: itemfirecat = 0
+    try:
+        itemdarksprite = pag.locateOnScreen('itemdarksprite.jpg', confidence=0.8)
+        cardlist.append(itemdarksprite)
+    except: itemdarksprite = 0
+    try:
+        itemimp = pag.locateOnScreen('itemimp.jpg', confidence=0.8)
+        cardlist.append(itemimp)
+    except: itemimp = 0
+    try:
+        itemstormsnake = pag.locateOnScreen('itemthundersnake.jpg', confidence=0.8)
+        cardlist.append(itemstormsnake)
+    except: itemstormsnake = 0
+    try:
+        itemfrostbeetle = pag.locateOnScreen('itemfrostbeetle.jpg', confidence=0.8)
+        cardlist.append(itemfrostbeetle)
+    except: itemfrostbeetle = 0
+
+    try:
+        itemscarab = pag.locateOnScreen('itemscarab.jpg', confidence=0.8)
+        cardlist.append(itemscarab)
+
+    except: itemscarab = 0
+
+    try:
+        itemheartbeat = pag.locateOnScreen('itemheartbeat.jpg', confidence=0.8)
+        cardlist.append(itemheartbeat)
+    except: itemheartbeat = 0
+
+    print(cardlist)
 
 
-def pathFinder():
+#def pathFinder():
 
     #Keep track of quest arrow direction and move based upon it.
 
@@ -33,24 +85,28 @@ def pathFinder():
     #If an interaction occurs, activate the interactionAutomator.
 
 
-def interactionAutomator():
+#def interactionAutomator():
 
     #Navigate interactions based upon what they are.
 
 
-def inventoryTracker():
+#def inventoryTracker():
 
     #Keep track of player inventory and be able to make comparisons of equipment.
 
     #Used for both equipment and drop rate data
 
 
-def dropTracker():
+#def dropTracker():
 
     #Use the inventoryTracker to make comparisons from the inventory.
 
     #Store the data inside a pandas dataframe, which will be passed onto the API.
 
-def botWrapper():
+#def botWrapper():
 
     #bundles the bot functions into one process.
+
+
+
+cardReader()
